9d2b9be56e6d1b5ef345d29b28a850a7c3859b96
Andrew Kim <andrewkim1@mail.weber.edu>
2022-05-05 21:36:18 -0600

    Added header with icons from MUI

---
+++ src/Home.js
@@ -0,0 +1,15 @@
+// This template was created using "rfce" from the "ES7+ Snippets" VSC extension.
+
+import React from 'react'
+import './Home.css' 
+
+function Home() {
+  return (
+    <div className='home'>
+        <h1>Home component</h1>
+        
+    </div>
+  )
+}
+
+export default Home
